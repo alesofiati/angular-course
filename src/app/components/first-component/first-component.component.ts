716b70f4b1ff6e15f1f17f6ba275fee0468bef74
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2023-01-08 22:12:39 -0300

    Primeiro component

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+
+export class FirstComponentComponent implements OnInit {
+  name:String = 'Alexandre Soffiatti Santos'
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
